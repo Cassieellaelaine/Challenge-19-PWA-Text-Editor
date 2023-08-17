e1a89f984683540d44e9462d722c605d51cba68b
Cassieellaelaine <129123349+Cassieellaelaine@users.noreply.github.com>
2023-08-16 22:06:33 -0400

    updated code

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,5 +1,4 @@
-// TODO: Create a service worker that caches static assets:
-// Vanilla service worker (no workbox)
+
 const CACHE_NAME = 'cache-v1';
 const urlsToCache = [ 
   '/',
@@ -11,19 +10,15 @@ const urlsToCache = [
   '/images/icon.png',
 ];
 precacheAndRoute(self.__WB_MANIFEST);
-// The three phases of the service worker's life cycle are:
-// 1. install
-// 2. activate
-// 3. claim
 
-// Install - the service worker is first installed and then activated.
+
 self.addEventListener('install', (e) =>
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   )
 );
 
-// Activate - the service worker is activated after install.
+
 self.addEventListener('activate', (e) =>
   e.waitUntil(
     caches.keys().then((keyList) =>
@@ -38,15 +33,12 @@ self.addEventListener('activate', (e) =>
   )
 );
 
-// Claim - the service worker is claimed after install.
-// When a service worker is initially registered, pages won't use it until the next load.
-// The clients.claim() method is used to claim the service worker immediately.
+
 self.addEventListener('activate', (e) => {
   e.waitUntil(clients.claim());
 });
 
-// Example of a simple cache-first network-first strategy
-// The service worker is checking the cache for a response and if it doesn't find it, it fetches it.
+
 self.addEventListener('fetch', (e) =>
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)))
 );
